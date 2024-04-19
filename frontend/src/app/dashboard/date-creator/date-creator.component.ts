809821490be83076afc3c8aa6659c9e83138396e
gmmonsal <gmmonsal@bancolombia.com.co>
2024-04-18 22:31:20 -0500

    Add new frontend components and styles

---
+++ frontend/src/app/dashboard/date-creator/date-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'date-creator',
+  templateUrl: './date-creator.component.html',
+  styleUrls: ['./date-creator.component.scss']
+})
+export class DateCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
