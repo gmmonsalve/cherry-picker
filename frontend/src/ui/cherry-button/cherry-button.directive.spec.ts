3fd3b0f34c800c337ed2a46a180f147aa6811b60
gmmonsal <gmmonsal@bancolombia.com.co>
2024-05-05 22:07:28 -0500

    feat: Add cherry-button directive and module

---
+++ frontend/src/ui/cherry-button/cherry-button.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CherryButtonDirective } from './cherry-button.directive';
+
+describe('CherryButtonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CherryButtonDirective();
+    expect(directive).toBeTruthy();
+  });
+});
