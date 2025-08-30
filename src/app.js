c017346b5d53c7d5a3d64622de9645e26c55a6cb
gmmonsalve <gbrielamonsalve@gmail.com>
2025-08-29 23:57:53 -0500

    feature(app): add storage and envelopeTemplate

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-
+import Store from './services/Store.js';
 
 const cherryPicker = {
     version: "1.0.0",
     name: "Cherry Picker",
     init: initApp,
     redirectToApp: renderAppContent,
+    store: new Store()
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,19 +14,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function initApp(){
     const Initbutton = document.getElementById('init-button')
-    Initbutton.addEventListener('click', ()=>{
-       cherryPicker.redirectToApp();
-    })
+    Initbutton.addEventListener('click', cherryPicker.redirectToApp)
+    cherryPicker.store.initializeStorage();
 }
 
 function renderAppContent(){
-    removeHeaderComponent();
+    const dateForm = document.getElementById("cherry-date-form");
+    const dateSection = document.getElementById("cherry-dates-container");
+    const envelopeTemplate = document.getElementById("date-envelope");
     const appContent = document.getElementById("app-content");
+
+
+    removeHeaderComponent();
     appContent.removeAttribute("hidden");
-    const addDateForm = document.getElementById("cherry-date-form");
-    addDateForm.addEventListener('submit', (event)=>{
-        event.preventDefault();
+
+    dateForm.addEventListener('submit', (event)=> onAddDate(event, envelopeTemplate, dateSection));
+
+    if(cherryPicker.store.getStoredDates().length > 0){
+        renderSavedDates(envelopeTemplate, dateSection);
+    }
+}
+
+function renderSavedDates(envelopeTemplate, dateSection){
+    const savedDates = cherryPicker.store.getStoredDates();
+    savedDates.forEach(date => {
+        const envelope = createEnvelope(envelopeTemplate, date);
+        dateSection.appendChild(envelope);
     });
+}
+
+
+function onAddDate(event, envelopeTemplate, dateSection){
+    event.preventDefault();
+    const dateTitle = event.target.elements[0].value;
+    if(dateTitle === "" || dateTitle === null) return;
+    const date = {id: `date-${Date.now()}`, title: dateTitle};
+    const envelope = createEnvelope(envelopeTemplate, date);
+    cherryPicker.store.addDate(date);
+    dateSection.appendChild(envelope);
+    event.target.elements[0].value = "";
+}
+
+function createEnvelope(envelopeTemplate, date){
+    const envelope = envelopeTemplate.content.cloneNode(true);
+    const dateContainer = envelope.querySelector('.cherry-date')
+    const dateTitleElement = envelope.getElementById("date-title");
+    dateContainer.id = date.id;
+    dateContainer.title = date.title;
+    dateTitleElement.textContent = date.title;
+    return envelope;
 }
 
 function removeHeaderComponent(){
